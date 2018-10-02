cd2e1a50f0703c2d0f478322b2d51759394cfcff
Kate Rentschler <nkpe@outlook.com>
2018-10-02 22:56:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your functions and code here
+var kittens = [
+  'Milo',
+  'Otis',
+  'Garfield'
+];
+
+/*var Arrays = [
+  'Milo',
+  'Otis',
+  'Garfield'
+];*/
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  //there is a testing here...ot fibal correct answer
+  kittens = kittens.splice(3, 0, name);
+  //kittens.splice(3, 1, name);
+  return kittens;
+}
